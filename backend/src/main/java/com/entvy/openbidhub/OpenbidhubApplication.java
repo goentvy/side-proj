fbf5f9f80b7dc1c53beed439134d95e1d029dab6
Entvy <entvyv@gmail.com>
2025-11-04 15:57:28 +0900

    feat(backend): initialize SpringBoot project

---
+++ backend/src/main/java/com/entvy/openbidhub/OpenbidhubApplication.java
@@ -0,0 +1,13 @@
+package com.entvy.openbidhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenbidhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenbidhubApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/entvy/openbidhub/OpenbidhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entvy.openbidhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenbidhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
